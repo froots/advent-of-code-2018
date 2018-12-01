12c2386bd7faaa236cb0c9610320fddcaad47980
Jim Newbery <james@tinnedfruit.com>
2018-12-01 16:29:39 +0000

    Solution 1B

--- ts/src/solutions/1/index.ts
+++ ts/src/solutions/1/index.ts
@@ -8,17 +8,40 @@ async function loadInput(n: number): Promise<string> {
 
 export async function solve(): Promise<void> {
   const input = parseInput(await loadInput(1));
-  console.log(solutionA(input));
+  console.log(`Part 1: ${solutionA(input)}`);
+  console.log(`Part 2: ${solutionB(input)}`);
 }
 
 function parseInput(input: string): number[] {
-  return input.split('\n').map(Number);
+  return input
+    .split('\n')
+    .map(line => line.trim())
+    .filter(line => line !== '')
+    .map(Number);
+}
+
+function add(a: number, b: number): number {
+  return a + b;
 }
 
 export function solutionA(frequencies: number[]): number {
   return frequencies.reduce(add, 0);
 }
 
-function add(a: number, b: number): number {
-  return a + b;
+export function solutionB(frequencies: number[]): number {
+  let total: number = 0;
+  let values: Set<number> = new Set([total]);
+
+  function findRepeatedFrequency(): number {
+    for (let n of frequencies) {
+      total += n;
+      if (values.has(total)) {
+        return total;
+      }
+      values.add(total);
+    }
+    return findRepeatedFrequency();
+  }
+ 
+  return findRepeatedFrequency();
 }
--- ts/tests/1.js
+++ ts/tests/1.js
@@ -1,8 +1,16 @@
 const test = require('tape');
-const { solutionA } = require('../dist/solutions/1');
+const { solutionA, solutionB } = require('../dist/solutions/1');
 
 test('Day 1A', t => {
   t.plan(2);
   t.equal(solutionA([1, -5, 7]), 3);
   t.equal(solutionA([-19, -12, -11, 16]), -26);
+});
+
+test('Day 1B', t => {
+  t.plan(4);
+  t.equal(solutionB([1, -1]), 0);
+  t.equal(solutionB([3, 3, 4, -2, -4]), 10);
+  t.equal(solutionB([-6, 3, 8, 5, -6]), 5);
+  t.equal(solutionB([7, 7, -2, -7, -4]), 14);
 });
