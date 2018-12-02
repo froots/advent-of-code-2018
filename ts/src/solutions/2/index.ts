4fd70c3b0050b16c3c8e8d1aa3ce3e73bfc9e2e3
Jim Newbery <james@tinnedfruit.com>
2018-12-02 16:50:02 +0000

    Split into sub-functions

--- ts/src/solutions/2/index.ts
+++ ts/src/solutions/2/index.ts
@@ -14,8 +14,27 @@ function parseInput(input: string): string[] {
 }
 
 export function solutionA(ids: string[]): number {
-  // map to interpreter
+  // Map each line to counts Map
+  // Map each line to array of [bool, bool] (2 letter, 3 letter groups present)
+  // reduce to single array [number, number] with counts of each
+  // return product of these
+
+  // const [twos, threes] = ids // string[]
+  //   .map(countChars) // Map<string, number>[]
+  //   .map(counts => [
+  //     hasExactCount(2, counts),
+  //     hasExactCount(3, counts)
+  //   ]) // boolean[][]
+  //   .reduce(sumBoolArray, [0, 0])
+  // return twos * threes;
+
   return ids.length;
+}
+
+export function countChars(inp: string): Map<string, number> {
+  return inp
+    .split('')
+    .reduce(characterCounter, new Map());
 }
 
 function characterCounter(
@@ -26,13 +45,16 @@ function characterCounter(
   return counts;
 }
 
-export function exactlyTwoOfAnyLetter(id: string): boolean {
-  let counts: Map<string, number> = id
-    .split('')
-    .reduce(characterCounter, new Map());
-  return [...counts.values()].some((count: number) => count === 2)
+export function hasExactCount(n: number, counts: Map<string, number>): boolean {
+  return [...counts.values()].some((count: number) => count === n);
 }
 
-export function exactlyThreeOfAnyLetter(id: string): boolean {
-  return true;
+export function sumBoolArray(arr: boolean[][]): number[] {
+  return arr
+    .map(vals => vals.map(v => Number(v)))
+    .reduce(arraySum, []);
 }
+
+function arraySum(a1: number[], a2: number[]): number[] {
+  return a2.map((n, i) => (a1[i] || 0) + n);
+} 
--- ts/tests/2.js
+++ ts/tests/2.js
@@ -1,16 +1,38 @@
 const test = require('tape');
-const { exactlyTwoOfAnyLetter } = require('../dist/solutions/2');
+const {
+  countChars,
+  hasExactCount,
+  sumBoolArray
+} = require('../dist/solutions/2');
 
-test('Exactly two of any letter', t => {
-  t.plan(10);
-  t.ok(exactlyTwoOfAnyLetter('aa'));
-  t.notOk(exactlyTwoOfAnyLetter('ab'));
-  t.notOk(exactlyTwoOfAnyLetter('aaa'));
-  t.notOk(exactlyTwoOfAnyLetter('abcdef'));
-  t.ok(exactlyTwoOfAnyLetter('bababc'));
-  t.ok(exactlyTwoOfAnyLetter('abbcde'));
-  t.notOk(exactlyTwoOfAnyLetter('abcccd'));
-  t.ok(exactlyTwoOfAnyLetter('aabcdd'));
-  t.ok(exactlyTwoOfAnyLetter('abcdee'));
-  t.notOk(exactlyTwoOfAnyLetter('ababab'));
+test('Counts characters in a string', t => {
+  t.plan(3);
+  let counts = countChars('aabcdd');
+  t.equal(counts.get('a'), 2);
+  t.equal(counts.get('b'), 1);
+  t.equal(counts.get('d'), 2);
+});
+
+test('Has exact count', t => {
+  t.plan(4);
+  let counts = new Map();
+  counts.set('a', 3);
+  counts.set('b', 2);
+  counts.set('c');
+  t.ok(hasExactCount(2, counts));
+  t.ok(hasExactCount(3, counts));
+  t.notOk(hasExactCount(4, counts));
+  t.notOk(hasExactCount(0, counts));
+});
+
+test('Sums array of booleans', t => {
+  t.plan(1);
+  let arr = [
+    [true, false],
+    [false, false],
+    [true, true],
+    [false, true],
+    [false, true]
+  ];
+  t.deepEqual(sumBoolArray(arr), [2, 3]);
 });
