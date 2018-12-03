6f1d26e2f95d957be7eb46106b30743de22f0bf7
Jim Newbery <james@tinnedfruit.com>
2018-12-03 20:43:30 +0000

    Select which day to run

--- ts/src/scripts/solve.ts
+++ ts/src/scripts/solve.ts
@@ -1,5 +1,17 @@
 import { solve as s1 } from '../solutions/1';
 import { solve as s2 } from '../solutions/2';
+import { solve as s3 } from '../solutions/3';
 
-s1();
-s2();
+const solutions = [
+  s1,
+  s2,
+  s3
+];
+
+if (process.argv[2]) {
+  const day = Number(process.argv[2]);
+  console.log(day);
+  solutions[day - 1]();
+} else {
+  solutions.forEach(solution => solution());
+}
