8bc2e35b29c94674d82aa747c96bd7c0f1871f9b
Jim Newbery <james@tinnedfruit.com>
2018-12-07 16:11:23 +0000

    Day 4 part 1 redux

--- ts/src/solutions/4/index.ts
+++ ts/src/solutions/4/index.ts
@@ -4,7 +4,7 @@ import { identity } from '../../identity';
 export async function solve(): Promise<void> {
   const input = parseInput(await loadInput(4));
   console.log(`Day 4, part 1: ${solutionA(input)}`);
-  console.log(`Day 4, part 2: ${solutionB(input)}`);
+  // console.log(`Day 4, part 2: ${solutionB(input)}`);
 }
 
 function parseInput(input: string): string[] {
@@ -15,183 +15,320 @@ function parseInput(input: string): string[] {
 }
 
 export function solutionA(input: string[]): number {
-  const sleepsByGuard: SleepsByGuard = prepareInput(input);
-  const guardWithMostSleeps: number = mostSleeps(sleepsByGuard);
-  const guardMostAsleep: number = mostAsleepTime(sleepsByGuard.get(guardWithMostSleeps));
-  return guardWithMostSleeps * guardMostAsleep;
+  const sleeps = [...input]
+    .sort(dateSorter)
+    .map(parseEvent)
+    .reduce(eventReducer, []);
+
+  const [idWithMostSleep] = [...totalsById(sleeps).entries()]
+    .reduce(maxValueReducer, [0, 0]);
+
+  const [minuteWithMostSleep] = sleeps
+    .filter(sleep => sleep.id === idWithMostSleep)
+    .reduce(sleepsByMinuteReducer, Array(60).fill(0))
+    .reduce(maxMinuteCountReducer, [-1, 0]);
+
+  return idWithMostSleep * minuteWithMostSleep;
 }
 
-export function solutionB(input: string[]): number {
-  const sleepsByGuard: SleepsByGuard = prepareInput(input);
- 
-  const sleepHeatMapByGuard: Map<number, [number, number]> = new Map();
-
-  for (let [id, sleeps] of sleepsByGuard) {
-    let heatMap: number[] = sleeps.reduce((hm: number[], sleep: Sleep): number[] => {
-      let newHeatMap = [...hm];
-      for (let i = sleep.startMinutes; i < sleep.endMinutes; i++) {
-        newHeatMap[i]++;
-      }
-      return newHeatMap;
-    }, Array(60).fill(0));
-
-    let maxAmount: number = Math.max(...heatMap);
-    let maxMinute: number = heatMap.indexOf(maxAmount);
-    
-    sleepHeatMapByGuard.set(id, [maxMinute, maxAmount]);
+function maxMinuteCountReducer(
+  [maxMinute, maxCount]: [number, number],
+  count: number,
+  minute: number
+): [number, number] {
+  if (count > maxCount) {
+    return [minute, count];
   }
+  return [maxMinute, maxCount];
+}
 
-  let maxId: number = -1;
-  let maxMinute: number = -1;
-  let maxAmount: number = 0;
-
-  for (let [id, [minute, amount]] of sleepHeatMapByGuard) {
-    if (amount > maxAmount) {
-      maxAmount = amount;
-      maxId = id;
-      maxMinute = minute;
-    }
+function sleepsByMinuteReducer(
+  minuteCounts: number[],
+  sleep: Sleep
+): number[] {
+  for (let i = sleep.start || 0; i < (sleep.end || 0); i++) {
+    minuteCounts[i]++;
   }
-
-  return maxId * maxMinute;
+  return minuteCounts;
 }
 
-function prepareInput(input: string[]): SleepsByGuard {
-  const sortedEntries = input
-    .map(parseEntry)
-    .sort(dateSorter);
-
-  const shifts = inferShifts(sortedEntries);
-  return guardSleeps(shifts);
-}
-
-export function parseEntry(entry: string): LogEntry {
-  const matcher = /\[(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2})\] (.+)/;
-  const matches: RegExpMatchArray | null = entry.match(matcher);
-  if (!matches) {
-    throw new Error('Could not parse entry');
+function maxValueReducer(
+  [maxId, maxVal]: [number, number],
+  [currentId, currentVal]: [number, number]
+): [number, number] {
+  if (currentVal > maxVal) {
+    return [currentId, currentVal];
   }
-  const datetime = new Date(
-    Number(matches[1]) + 500, // Avoiding historical date corrections
-    Number(matches[2]) - 1,
-    Number(matches[3]),
-    Number(matches[4]),
-    Number(matches[5])
-  );
-  const message = matches[6] || '';
-  return { datetime, message };
+  return [maxId, maxVal];
 }
 
-export function dateSorter(a: LogEntry, b: LogEntry): number {
-  return a.datetime.getTime() - b.datetime.getTime();
-}
-
-export function inferShifts(entries: LogEntry[]): Shift[] {
-  let currentShift: Shift = new Shift();
-  let currentSleep: Sleep = new Sleep();
-  let shifts: Shift[] = []; 
-
-  for (let entry of entries) {
-    switch (entry.message) {
-      case 'falls asleep':
-        currentSleep.startMinutes = entry.datetime.getMinutes();
-        break;
-      case 'wakes up':
-        currentSleep.endMinutes = entry.datetime.getMinutes();
-        currentShift.sleeps = [...currentShift.sleeps, currentSleep];
-        currentSleep = new Sleep();
-        break;
-      default:
-        const guardIdMatches: RegExpMatchArray | null = entry.message.match(/Guard #(\d+) begins shift/);
-        if (!guardIdMatches) {
-          throw new Error('Can not parse shift');
-        }
-        if (currentShift.guardId !== 0) {
-          shifts = [...shifts, currentShift];
-        }
-        currentShift = new Shift();
-        currentShift.guardId = Number(guardIdMatches[1]);
-        currentShift.date = new Date(entry.datetime.getTime())
-        if (currentShift.date.getHours() >= 12) {
-          currentShift.date.setDate(currentShift.date.getDate() + 1);
-        }
-        currentShift.date.setHours(0);
-        currentShift.date.setMinutes(0);
-        break;
-    }
-  }
-
-  if (currentShift.guardId !== 0) {
-    shifts = [...shifts, currentShift];
-  }
-
-  return shifts;
-}
-
-export function guardSleeps(shifts: Shift[]): SleepsByGuard {
-  let sleepsByGuard = new Map();
-  for (let shift of shifts) {
-    sleepsByGuard.set(
-      shift.guardId,
-      [...(sleepsByGuard.get(shift.guardId) || []), ...shift.sleeps]
-    );
-  }
-  return sleepsByGuard;
-}
-
-export function mostSleeps(sleepsByGuard: SleepsByGuard): number {
-  let guardTotals: Map<number, number> = new Map;
-  for (let [id, sleeps] of sleepsByGuard) {
-    const totalSleep = sleeps.reduce((total: number, sleep: Sleep): number => {
-      return total + sleep.endMinutes - sleep.startMinutes;
-    }, 0);
-    guardTotals.set(id, totalSleep);
-  }
-
-  const totals = Array.from(guardTotals.entries());
-
-  const sorter = ([id1, tot1]: [number, number], [id2, tot2]: [number, number]) => {
-    return tot2 - tot1;
-  };
-
-  return totals.sort(sorter)[0][0];
-}
-
-export function mostAsleepTime(sleeps: Sleep[] = []): number {
-  const minutes = Array(60).fill(0);
+export function totalsById(sleeps: Sleep[]): Map<number, number> {
+  let totals: Map<number, number> = new Map();
   for (let sleep of sleeps) {
-    for (let i: number = sleep.startMinutes; i < sleep.endMinutes; i++) {
-      minutes[i]++;
+    let total = 
+      (totals.get(sleep.id) || 0) +
+      (sleep.end || 0) -
+      (sleep.start || 0);
+    totals.set(sleep.id, total);
+  }
+  return totals;
+}
+
+export function eventReducer(sleeps: Sleep[], event: Event): Sleep[] {
+  let sleep: Sleep | undefined;
+  switch (event.type) {
+    case EventType.BEGINS_SHIFT:
+      sleep = {
+        id: event.payload
+      };
+      return [...sleeps, sleep];
+
+    case EventType.FALLS_ASLEEP:
+      sleep = sleeps.pop();
+      if (!sleep) {
+        return sleeps;
+      }
+      if (sleep.end && sleep.end > 0) {
+        return [
+          ...sleeps,
+          sleep,
+          { 
+            id: sleep.id,
+            start: event.payload
+          }
+        ];
+      }
+      sleep.start = event.payload;
+      return [...sleeps, sleep];
+
+    case EventType.WAKES_UP:
+      sleep = sleeps.pop();
+      if (!sleep) {
+        return sleeps;
+      }
+      sleep.end = event.payload;
+      return [...sleeps, sleep];
+
+    default:
+      return [...sleeps];
+  }
+}
+
+export function parseEvent(entry: string): Event {
+  if (entry.includes('begins shift')) {
+    let idMatch = entry.match(/#\d+/);
+    if (!idMatch) {
+      throw new Error('Can not parse');
+    }
+    return {
+      type: EventType.BEGINS_SHIFT,
+      payload: Number(idMatch[0].slice(1))
     }
   }
-  return minutes.indexOf(Math.max(...minutes));
-}
-
-export class Shift {
-  guardId: number;
-  date: Date;
-  sleeps: Sleep[];
-
-  constructor(guardId: number = 0, date: Date = new Date(), sleeps: Sleep[] = []) {
-    this.guardId = guardId;
-    this.date = date;
-    this.sleeps = sleeps;
+  if (entry.includes('falls asleep')) {
+    return {
+      type: EventType.FALLS_ASLEEP,
+      payload: Number(entry.slice(15, 17))
+    };
   }
-}
-
-export class Sleep {
-  startMinutes: number;
-  endMinutes: number;
-
-  constructor(startMinutes: number = 0, endMinutes: number = 0) {
-    this.startMinutes = startMinutes;
-    this.endMinutes = endMinutes;
+  if (entry.includes('wakes up')) {
+    return {
+      type: EventType.WAKES_UP,
+      payload: Number(entry.slice(15, 17))
+    };
   }
+  throw new Error('Unknown event type');
 }
 
-type SleepsByGuard = Map<number, Sleep[]>;
+export function dateSorter(a: string, b: string): number {
+  const dateA = a.substring(1, 17);
+  const dateB = b.substring(1, 17);
+  if (dateA < dateB) {
+    return -1;
+  }
+  if (dateA > dateB) {
+    return 1;
+  }
+  return 0;
+}
 
-type LogEntry = {
-  datetime: Date;
-  message: string;
+// export function solutionA(input: string[]): number {
+//   const sleepsByGuard: SleepsByGuard = prepareInput(input);
+//   const guardWithMostSleeps: number = mostSleeps(sleepsByGuard);
+//   const guardMostAsleep: number = mostAsleepTime(sleepsByGuard.get(guardWithMostSleeps));
+//   return guardWithMostSleeps * guardMostAsleep;
+// }
+
+// export function solutionB(input: string[]): number {
+//   const sleepsByGuard: SleepsByGuard = prepareInput(input);
+ 
+//   const sleepHeatMapByGuard: Map<number, [number, number]> = new Map();
+
+//   for (let [id, sleeps] of sleepsByGuard) {
+//     let heatMap: number[] = sleeps.reduce((hm: number[], sleep: Sleep): number[] => {
+//       let newHeatMap = [...hm];
+//       for (let i = sleep.startMinutes; i < sleep.endMinutes; i++) {
+//         newHeatMap[i]++;
+//       }
+//       return newHeatMap;
+//     }, Array(60).fill(0));
+
+//     let maxAmount: number = Math.max(...heatMap);
+//     let maxMinute: number = heatMap.indexOf(maxAmount);
+    
+//     sleepHeatMapByGuard.set(id, [maxMinute, maxAmount]);
+//   }
+
+//   let maxId: number = -1;
+//   let maxMinute: number = -1;
+//   let maxAmount: number = 0;
+
+//   for (let [id, [minute, amount]] of sleepHeatMapByGuard) {
+//     if (amount > maxAmount) {
+//       maxAmount = amount;
+//       maxId = id;
+//       maxMinute = minute;
+//     }
+//   }
+
+//   return maxId * maxMinute;
+// }
+
+// export function parseEntry(entry: string): LogEntry {
+//   const matcher = /\[(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2})\] (.+)/;
+//   const matches: RegExpMatchArray | null = entry.match(matcher);
+//   if (!matches) {
+//     throw new Error('Could not parse entry');
+//   }
+//   const datetime = new Date(
+//     Number(matches[1]) + 500, // Avoiding historical date corrections
+//     Number(matches[2]) - 1,
+//     Number(matches[3]),
+//     Number(matches[4]),
+//     Number(matches[5])
+//   );
+//   const message = matches[6] || '';
+//   return { datetime, message };
+// }
+
+// export function inferShifts(entries: LogEntry[]): Shift[] {
+//   let currentShift: Shift = new Shift();
+//   let currentSleep: Sleep = new Sleep();
+//   let shifts: Shift[] = []; 
+
+//   for (let entry of entries) {
+//     switch (entry.message) {
+//       case 'falls asleep':
+//         currentSleep.startMinutes = entry.datetime.getMinutes();
+//         break;
+//       case 'wakes up':
+//         currentSleep.endMinutes = entry.datetime.getMinutes();
+//         currentShift.sleeps = [...currentShift.sleeps, currentSleep];
+//         currentSleep = new Sleep();
+//         break;
+//       default:
+//         const guardIdMatches: RegExpMatchArray | null = entry.message.match(/Guard #(\d+) begins shift/);
+//         if (!guardIdMatches) {
+//           throw new Error('Can not parse shift');
+//         }
+//         if (currentShift.guardId !== 0) {
+//           shifts = [...shifts, currentShift];
+//         }
+//         currentShift = new Shift();
+//         currentShift.guardId = Number(guardIdMatches[1]);
+//         currentShift.date = new Date(entry.datetime.getTime())
+//         if (currentShift.date.getHours() >= 12) {
+//           currentShift.date.setDate(currentShift.date.getDate() + 1);
+//         }
+//         currentShift.date.setHours(0);
+//         currentShift.date.setMinutes(0);
+//         break;
+//     }
+//   }
+
+//   if (currentShift.guardId !== 0) {
+//     shifts = [...shifts, currentShift];
+//   }
+
+//   return shifts;
+// }
+
+// export function guardSleeps(shifts: Shift[]): SleepsByGuard {
+//   let sleepsByGuard = new Map();
+//   for (let shift of shifts) {
+//     sleepsByGuard.set(
+//       shift.guardId,
+//       [...(sleepsByGuard.get(shift.guardId) || []), ...shift.sleeps]
+//     );
+//   }
+//   return sleepsByGuard;
+// }
+
+// export function mostSleeps(sleepsByGuard: SleepsByGuard): number {
+//   let guardTotals: Map<number, number> = new Map;
+//   for (let [id, sleeps] of sleepsByGuard) {
+//     const totalSleep = sleeps.reduce((total: number, sleep: Sleep): number => {
+//       return total + sleep.endMinutes - sleep.startMinutes;
+//     }, 0);
+//     guardTotals.set(id, totalSleep);
+//   }
+
+//   const totals = Array.from(guardTotals.entries());
+
+//   const sorter = ([id1, tot1]: [number, number], [id2, tot2]: [number, number]) => {
+//     return tot2 - tot1;
+//   };
+
+//   return totals.sort(sorter)[0][0];
+// }
+
+// export function mostAsleepTime(sleeps: Sleep[] = []): number {
+//   const minutes = Array(60).fill(0);
+//   for (let sleep of sleeps) {
+//     for (let i: number = sleep.startMinutes; i < sleep.endMinutes; i++) {
+//       minutes[i]++;
+//     }
+//   }
+//   return minutes.indexOf(Math.max(...minutes));
+// }
+
+// export class Shift {
+//   guardId: number;
+//   date: Date;
+//   sleeps: Sleep[];
+
+//   constructor(guardId: number = 0, date: Date = new Date(), sleeps: Sleep[] = []) {
+//     this.guardId = guardId;
+//     this.date = date;
+//     this.sleeps = sleeps;
+//   }
+// }
+
+// export class Sleep {
+//   startMinutes: number;
+//   endMinutes: number;
+
+//   constructor(startMinutes: number = 0, endMinutes: number = 0) {
+//     this.startMinutes = startMinutes;
+//     this.endMinutes = endMinutes;
+//   }
+// }
+
+// type SleepsByGuard = Map<number, Sleep[]>;
+
+type Event = {
+  type: EventType;
+  payload: number;
+}
+
+enum EventType {
+  BEGINS_SHIFT,
+  FALLS_ASLEEP,
+  WAKES_UP
+}
+
+type Sleep = {
+  id: number;
+  start?: number;
+  end?: number;
 }
--- ts/tests/4.js
+++ ts/tests/4.js
@@ -1,10 +1,12 @@
 const test = require('tape');
 const {
   solutionA,
-  solutionB,
-  parseEntry,
   dateSorter,
-  inferShifts,
+  solutionB,
+  parseEvent,
+  eventReducer,
+  totalsById,
+
   Shift,
   Sleep,
   guardSleeps,
@@ -33,122 +35,133 @@ let input = [
 
 test('Day 4 part 1', t => {
   t.plan(1);
+  const solution = solutionA(input);
+  t.comment(JSON.stringify(solution));
   t.equal(solutionA(input), 240);
 });
 
-test('Day 4 parse entry', t => {
-  t.plan(4);
-  const entry1 = '[1518-11-01 23:58] Guard #99 begins shift';
-  const entry2 = '[1518-11-03 00:24] falls asleep';
-
-  const parsedEntry1 = parseEntry(entry1);
-  const parsedEntry2 = parseEntry(entry2);
-
-  t.equal(parsedEntry1.datetime.toISOString(), '2018-11-01T23:58:00.000Z'); // shifting by 500 years to avoid nastiness with corrected times
-  t.equal(parsedEntry1.message, 'Guard #99 begins shift');
-  t.equal(parsedEntry2.datetime.toISOString(), '2018-11-03T00:24:00.000Z');
-  t.equal(parsedEntry2.message, 'falls asleep');
-});
-
 test('Day 4 date sorter', t => {
+  t.plan(2);
+  const sorted = [...input].sort(dateSorter);
+  t.equal(sorted[0], input[1]);
+  t.equal(sorted[1], input[0]);
+});
+
+test('Day 4 parse event', t => {
+  t.plan(6);
+  const shiftStart = parseEvent(input[1]);
+  const fallsAsleep = parseEvent(input[0]);
+  const wakesUp = parseEvent(input[4]);
+
+  t.equal(shiftStart.type, 0); // Should be using TS for these test I guess
+  t.equal(shiftStart.payload, 10);
+  t.equal(fallsAsleep.type, 1);
+  t.equal(fallsAsleep.payload, 5);
+  t.equal(wakesUp.type, 2);
+  t.equal(wakesUp.payload, 25);
+});
+
+test('Day 4 #eventReducer shift start', t => {
+  t.plan(1);
+  const shiftStart = eventReducer([], { type: 0, payload: 10 });
+  const expected = [{ id: 10 }];
+  t.deepEqual(
+    shiftStart,
+    expected,
+    'Starting a shift creates the first sleep entry'
+  );
+});
+
+test('Day 4 #eventReducer shift start with previous sleep', t => {
+  t.plan(1);
+  const sleep1 = { id: 99, start: 30, end: 40 };
+  const shiftStart = eventReducer([sleep1], { type: 0, payload: 10 });
+  const expected = [sleep1, { id: 10 }];
+  t.deepEqual(
+    shiftStart,
+    expected,
+    'Starting a shift creates a new sleep entry'
+  );
+});
+
+test('Day 4 #eventReducer falls asleep', t => {
+  t.plan(1);
+  const fallsAsleep = eventReducer([{ id: 10 }], { type: 1, payload: 5 });
+  const expected = [{ id: 10, start: 5 }];
+  t.deepEqual(
+    fallsAsleep,
+    expected,
+    'Falling asleep sets the start time on the current sleep'
+  );
+});
+
+test('Day 4 #eventReducer falls asleep again', t => {
+  t.plan(1);
+  const sleep1 = { id: 10, start: 5, end: 25 };
+  const fallsAsleep2 = eventReducer([sleep1], { type: 1, payload: 30 });
+  const expected = [sleep1, { id: 10, start: 30 }];
+  t.deepEqual(
+    fallsAsleep2,
+    expected,
+    'Falling asleep again creates a new sleep entry with the same id and a start time'
+  );
+});
+
+test('Day 4 #eventReducer wakes up', t => {
+  t.plan(1);
+  const wakesUp = eventReducer([{ id: 10, start: 5 }], {
+    type: 2,
+    payload: 25
+  });
+  const expected = [{ id: 10, start: 5, end: 25 }];
+  t.deepEqual(
+    wakesUp,
+    expected,
+    'Waking up sets the end time on the current sleep'
+  );
+});
+
+test('Day 4 #totalsById', t => {
   t.plan(3);
-
-  const date1 = new Date(2018, 1, 22, 23, 5);
-  const date2 = new Date(2018, 1, 22, 22, 4);
-  const date3 = new Date(2018, 1, 22, 22, 31);
-  const entries = [
-    { datetime: date1 },
-    { datetime: date2 },
-    { datetime: date3 }
+  const sleeps = [
+    { id: 1, start: 5, end: 10 },
+    { id: 1, start: 25, end: 50 },
+    { id: 2, start: 30, end: 40 },
+    { id: 3, start: 50, end: 55 }
   ];
-
-  const sorted = entries.sort(dateSorter);
-
-  t.equal(sorted[0].datetime, date2);
-  t.equal(sorted[1].datetime, date3);
-  t.equal(sorted[2].datetime, date1);
+  const actual = totalsById(sleeps);
+  t.equal(actual.get(1), 30);
+  t.equal(actual.get(2), 10);
+  t.equal(actual.get(3), 5);
 });
 
-test('Day 4 #inferShifts', t => {
-  t.plan(10);
+// test('Day 4 #guardSleeps', t => {
+//   t.plan(4);
 
-  const entries = [
-    {
-      datetime: new Date(2018, 10, 1, 0, 3),
-      message: 'Guard #10 begins shift'
-    },
-    {
-      datetime: new Date(2018, 10, 1, 0, 5),
-      message: 'falls asleep'
-    },
-    {
-      datetime: new Date(2018, 10, 1, 0, 25),
-      message: 'wakes up'
-    },
-    {
-      datetime: new Date(2018, 10, 1, 0, 30),
-      message: 'falls asleep'
-    },
-    {
-      datetime: new Date(2018, 10, 1, 0, 55),
-      message: 'wakes up'
-    },
-    {
-      datetime: new Date(2018, 10, 1, 23, 58),
-      message: 'Guard #99 begins shift'
-    },
-    {
-      datetime: new Date(2018, 10, 2, 0, 40),
-      message: 'falls asleep'
-    },
-    {
-      datetime: new Date(2018, 10, 2, 0, 50),
-      message: 'wakes up'
-    }
-  ];
-  const shifts = inferShifts(entries);
+//   const s1 = new Shift(10, new Date(2018, 10, 1), [
+//     new Sleep(5, 25),
+//     new Sleep(30, 55)
+//   ]);
+//   const s2 = new Shift(99, new Date(2018, 10, 2), [new Sleep(40, 50)]);
+//   const s3 = new Shift(10, new Date(2018, 10, 3), [new Sleep(24, 29)]);
+//   const s4 = new Shift(99, new Date(2018, 10, 4), [new Sleep(36, 46)]);
+//   const s5 = new Shift(99, new Date(2018, 10, 5), [new Sleep(45, 55)]);
+//   const shifts = [s1, s2, s3, s4, s5];
+//   const sleepsByGuard = guardSleeps(shifts);
 
-  t.equal(shifts.length, 2);
+//   t.equal(sleepsByGuard.size, 2);
+//   t.equal(sleepsByGuard.get(10).length, 3);
+//   t.equal(sleepsByGuard.get(10)[0].startMinutes, 5);
+//   t.equal(sleepsByGuard.get(10)[0].endMinutes, 25);
+// });
 
-  t.equal(shifts[0].guardId, 10);
-  t.equal(shifts[0].date.getTime(), new Date(2018, 10, 1).getTime());
-  t.equal(shifts[0].sleeps.length, 2);
-  t.deepEqual(shifts[0].sleeps[0], { startMinutes: 5, endMinutes: 25 });
-  t.deepEqual(shifts[0].sleeps[1], { startMinutes: 30, endMinutes: 55 });
+// test('Day 4 most asleep time', t => {
+//   t.plan(1);
+//   const sleeps = [new Sleep(5, 25), new Sleep(30, 55), new Sleep(24, 29)];
+//   t.equal(mostAsleepTime(sleeps), 24);
+// });
 
-  t.equal(shifts[1].guardId, 99);
-  t.equal(shifts[1].date.getTime(), new Date(2018, 10, 2).getTime());
-  t.equal(shifts[1].sleeps.length, 1);
-  t.deepEqual(shifts[1].sleeps[0], { startMinutes: 40, endMinutes: 50 });
-});
-
-test('Day 4 #guardSleeps', t => {
-  t.plan(4);
-
-  const s1 = new Shift(10, new Date(2018, 10, 1), [
-    new Sleep(5, 25),
-    new Sleep(30, 55)
-  ]);
-  const s2 = new Shift(99, new Date(2018, 10, 2), [new Sleep(40, 50)]);
-  const s3 = new Shift(10, new Date(2018, 10, 3), [new Sleep(24, 29)]);
-  const s4 = new Shift(99, new Date(2018, 10, 4), [new Sleep(36, 46)]);
-  const s5 = new Shift(99, new Date(2018, 10, 5), [new Sleep(45, 55)]);
-  const shifts = [s1, s2, s3, s4, s5];
-  const sleepsByGuard = guardSleeps(shifts);
-
-  t.equal(sleepsByGuard.size, 2);
-  t.equal(sleepsByGuard.get(10).length, 3);
-  t.equal(sleepsByGuard.get(10)[0].startMinutes, 5);
-  t.equal(sleepsByGuard.get(10)[0].endMinutes, 25);
-});
-
-test('Day 4 most asleep time', t => {
-  t.plan(1);
-  const sleeps = [new Sleep(5, 25), new Sleep(30, 55), new Sleep(24, 29)];
-  t.equal(mostAsleepTime(sleeps), 24);
-});
-
-test('Day 4 part 2', t => {
-  t.plan(1);
-  t.equal(solutionB(input), 4455);
-});
+// test('Day 4 part 2', t => {
+//   t.plan(1);
+//   t.equal(solutionB(input), 4455);
+// });
