dc0744dcf22a325fcf4ac506b301398c51735e25
Jim Newbery <james@tinnedfruit.com>
2018-12-07 19:02:00 +0000

    Find bounds and get all points in those bounds

--- ts/src/solutions/6/index.ts
+++ ts/src/solutions/6/index.ts
@@ -14,6 +14,40 @@ function parseInput(input: string): number[][] {
     .map(line => line.split(', ').map(Number));
 }
 
-export function solutionA(coords: number[][]): number {
-  return 17;
+export function solutionA(input: number[][]) {
+  let [topLeft, bottomRight] = findBounds(input);
+  let points = allPoints(topLeft, bottomRight); 
+  return points;
 }
+
+export function allPoints(
+  [x1, y1]: number[],
+  [x2, y2]: number[]
+): number[][] {
+  let points = [];
+  for (let y = y1; y <= y2; y++) {
+    for (let x = x1; x <= x2; x++) {
+      points.push([x, y]);
+    }
+  }
+  return points;
+}
+
+export function findBounds(coords: number[][]): number[][] {
+  return [
+    [
+      Math.min(...pluckIndex(coords, 0)),
+      Math.min(...pluckIndex(coords, 1))
+    ],
+    [
+      Math.max(...pluckIndex(coords, 0)),
+      Math.max(...pluckIndex(coords, 1))
+    ]
+  ];
+}
+
+function pluckIndex(collection: any[][], idx: number) {
+  return collection.map(inner => inner[idx]);
+}
+
+
--- ts/src/solutions/6/tests.ts
+++ ts/src/solutions/6/tests.ts
@@ -1,5 +1,5 @@
 import tape from 'tape';
-import { solutionA } from './';
+import { solutionA, findBounds, allPoints } from './';
 
 const input = [
   [1, 1],
@@ -12,5 +12,23 @@ const input = [
 
 tape('Day 6 part 1', t => {
   t.plan(1);
+  t.comment(solutionA(input));
   t.equal(solutionA(input), 17);
 });
+
+tape('Day 6 #findBounds', t => {
+  t.plan(1);
+  t.deepEqual(findBounds(input), [[1, 1], [8, 9]]);
+})
+
+tape('Day 6 #allPoints', t => {
+  t.plan(1);
+  t.deepEqual(allPoints([3, 4], [5, 5]), [
+    [3, 4],
+    [4, 4],
+    [5, 4],
+    [3, 5],
+    [4, 5],
+    [5, 5]
+  ]);
+})
