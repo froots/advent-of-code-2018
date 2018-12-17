229d8ff2de56f2f4f0859d4505273d201e9f49e6
Jim Newbery <james@tinnedfruit.com>
2018-12-17 21:23:48 +0000

    Day 11, part 2 solution

--- ts/src/solutions/11/index.ts
+++ ts/src/solutions/11/index.ts
@@ -1,12 +1,17 @@
 export async function solve(): Promise<void> {
   const input = 5153;
+  console.time('part1');
   console.log(`Day 11, part 1: ${solutionA(input)}`);
+  console.timeEnd('part1');
+  console.time('part2');
+  console.log(`Day 11, part 2: ${solutionB(input)}`);
+  console.timeEnd('part2');
 }
 
 export function solutionA(serialNo: number): string {
   let g = grid(serialNo, 300, 300)
-    .map((row, y) => row.map((cell, x) => cellPower(x + 1, y + 1, cell)))
-    .map((row, y, cells) => row.map((cell, x) => sum(cells, 3, x, y)));
+    .map(powerMapper)
+    .map(sumMapper3);
   
   let max = -Infinity;
   let maxX = -1;
@@ -24,6 +29,59 @@ export function solutionA(serialNo: number): string {
 
   return `${maxX + 1},${maxY + 1}`;
 }
+
+export function solutionB(serialNo: number, size: number = 300): string {
+  let g = grid(serialNo, size, size)
+    .map(powerMapper);
+  let summed = summedArea(g);
+  let candidates: Candidate[] = [];
+
+  summed.forEach((row: number[], y: number, cells: number[][]) => {
+    row.forEach((sa: number, x: number) => {
+      const maxSize = cells.length - Math.max(x, y);
+      candidates.push({x: x + 1, y: y + 1, size: 1, total: cells[y][x]});
+      for (let size = 2; size <= maxSize; size++) {
+        let A = (x > 0 && y > 0) ? cells[y - 1][x - 1] : 0;
+        let B = (y > 0) ? cells[y - 1][x + size - 1] : 0;
+        let C = (x > 0) ? cells[y + size - 1][x - 1] : 0;
+        let D = cells[y + size - 1][x + size - 1];
+        let total = D + A - B - C;
+        candidates.push({x: x + 1, y: y + 1, size, total});
+      }
+    });
+  });
+
+  const biggest: Candidate = candidates
+    .reduce((max, candidate) => 
+      (candidate.total > max.total) ? candidate : max,
+      { x: -1, y: -1, size: 0, total: -Infinity }
+    );
+
+  return `${biggest.x},${biggest.y},${biggest.size}`;
+}
+
+function summedArea(g: number[][]): number[][] {
+  let sa = grid(0, g.length, g.length);
+
+  g.forEach((row: number[], y: number) => {
+    row.forEach((cell: number, x: number) => {
+      sa[y][x] = g[y][x] +
+        (y > 0 ? sa[y - 1][x] : 0) +
+        (x > 0 ? sa[y][x - 1] : 0) -
+        (y > 0 && x > 0 ? sa[y - 1][x - 1] : 0);
+    });
+  });
+
+  return sa;
+}
+
+function powerMapper(row: number[], y: number) {
+  return row.map((cell, x) => cellPower(x + 1, y + 1, cell))
+}
+
+function sumMapper3(row: number[], y: number, cells: number[][]) {
+  return row.map((cell, x) => sum(cells, 3, x, y));
+} 
 
 function sum(g: number[][], size: number, startX: number, startY: number) {
   let total = 0;
@@ -51,3 +109,10 @@ export function cellPower(x: number, y: number, serial: number): number {
     0;
   return power - 5;
 }
+
+type Candidate = {
+  x: number,
+  y: number,
+  size: number,
+  total: number
+};
--- ts/src/solutions/11/tests.ts
+++ ts/src/solutions/11/tests.ts
@@ -1,5 +1,5 @@
 import tape from 'tape';
-import { solutionA } from './';
+import { solutionA, solutionB, cellPower} from './';
 
 tape('Day 11 part 1', t => {
   t.plan(2);
@@ -7,35 +7,15 @@ tape('Day 11 part 1', t => {
   t.equal(solutionA(42), '21,61');
 });
 
-// tape('Day 11 #cellPower', t => {
-//   t.plan(3);
-//   t.equal(cellPower(3, 5, 8), 4);
-//   t.equal(cellPower(122, 79, 57), -5);
-//   t.equal(cellPower(101, 153, 71), 4);
-// });
+tape('Day 11 part 2', t => {
+  t.plan(2);
+  t.equal(solutionB(18), '90,269,16');
+  t.equal(solutionB(42), '232,251,12');
+})
 
-// tape('Day 11 #Grid constructor', t => {
-//   t.plan(2);
-//   const g = new Grid(18);
-//   t.equal(g.cells.length, 300);
-//   t.equal(g.cells[0].length, 300);
-// });
-
-// tape('Day 11 #Grid calculate', t => {
-//   t.plan(3);
-//   const g = new Grid(18);
-//   g.powerUp();
-//   t.equal(g.cells[0][0], -2);
-//   t.equal(g.cells[44][32], 4);
-//   t.equal(g.cells[299][299], 0);
-// });
-
-// tape('Day 11 #Grid bestRegion', t => {
-//   t.plan(2);
-//   let g1 = new Grid(18);
-//   g1.powerUp();
-//   t.deepEqual(g1.bestRegion(), [33, 45]);
-//   let g2 = new Grid(42);
-//   g2.powerUp();
-//   t.deepEqual(g2.bestRegion(), [21, 61]);
-// });
+tape('Day 11 #cellPower', t => {
+  t.plan(3);
+  t.equal(cellPower(3, 5, 8), 4);
+  t.equal(cellPower(122, 79, 57), -5);
+  t.equal(cellPower(101, 153, 71), 4);
+});
