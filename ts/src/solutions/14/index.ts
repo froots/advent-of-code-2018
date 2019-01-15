454f1b4d48cd7ffeb094bb63aa52fce760a48c17
Jim Newbery <james@tinnedfruit.com>
2019-01-15 21:02:06 +0000

    Day 14 step

--- ts/src/solutions/14/index.ts
+++ ts/src/solutions/14/index.ts
@@ -5,9 +5,31 @@ export function solve(): void {
 }
 
 export function solutionA(recipes: number): number {
+  const startState: State = {
+    recipes: [3, 7],
+    pointers: [0, 1]
+  };
   return 5158916779;
+}
+
+export function step(state: State): State {
+  let [p1, p2] = state.pointers;
+  const r1 = state.recipes[p1];
+  const r2 = state.recipes[p2];
+  const recipes = [...state.recipes, ...digitSum(r1, r2)];
+  p1 = p1 + (r1 + 1) % recipes.length;
+  p2 = p2 + (r2 + 1) % recipes.length;
+  return {
+    recipes,
+    pointers: [p1, p2]
+  };
 }
 
 export function digitSum(n1: number, n2: number): number[] {
   return (n1 + n2).toString().split('').map(Number);
 }
+
+export type State = {
+  recipes: number[],
+  pointers: [number, number]
+};
--- ts/src/solutions/14/tests.ts
+++ ts/src/solutions/14/tests.ts
@@ -1,5 +1,5 @@
 import tape from 'tape';
-import { solutionA, digitSum } from './';
+import { solutionA, digitSum, State, step } from './';
 
 tape('Day 14 Part 1', t => {
   t.plan(1);
@@ -14,4 +14,21 @@ tape('Day 14 digitSum', t => {
   t.deepEqual(digitSum(0, 1), [1]);
   t.deepEqual(digitSum(6, 4), [1, 0]);
   t.deepEqual(digitSum(2, 9), [1, 1]);
+});
+
+tape.only('Day 14 step', t => {
+  t.plan(6);
+  let state: State = {
+    recipes: [3, 7],
+    pointers: [0, 1]
+  };
+  state = step(state);
+  t.deepEqual(state.recipes, [3, 7, 1, 0], 'recipes1');
+  t.deepEqual(state.pointers, [0, 1], 'pointers1');
+  state = step(state);
+  t.deepEqual(state.recipes, [3, 7, 1, 0, 1, 0], 'recipes2');
+  t.deepEqual(state.pointers, [4, 3], 'pointers2');
+  state = step(state);
+  t.deepEqual(state.recipes, [3, 7, 1, 0, 1, 0, 1], 'recipes3');
+  t.deepEqual(state.pointers, [6, 4], 'pointers3');
 });
